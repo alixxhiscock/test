1ed6fb176f2e1671adaff8c6a2e4b339ad172c41
alixxhiscock <A.Hiscock2492@student.leedsbeckett.ac.uk>
2023-10-19 09:07:59 +0100

    sdds

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 #!/usr/bin/env python3
 
-class Dog:
-    def __init__(self,name, color):
-        self.color = color
-        self.name = name
-    def speak(self):
-        print(f"my name is {self.name}")
-
-if __name__ == '__main__'//////////////
-
-    peter = Dog("Peter", "Black")
-    peter.speak()
+class Cat:
